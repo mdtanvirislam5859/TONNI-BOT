79c22f68b75a8ed22abd60fc68562c1d54cfadd6
Md Tanvir Islam <mdtanvirislam5859@gmail.com>
2024-06-12 15:03:23 +0600

    Update love_video.js

--- modules/commands/love_video.js
+++ modules/commands/love_video.js
@@ -1,6 +1,6 @@
 /** I am doing this coding with a lot of difficulty, please don't post it yourself¯\_(ツ)_/¯ **/
 module.exports.config = {
-  name: "love",
+  name: "lovesaim",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "Badol",
